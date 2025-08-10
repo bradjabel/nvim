40b47f7c16e73ae63f9d6aaacef039a8b6033a7f
unknown <brad.j.abel@gmail.com>
2025-08-10 12:27:48 -0600

    telescope diagnostics builtin

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,31 +1,32 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
-      require('telescope').setup {
+      require("telescope").setup({
         defaults = {
           layout_config = {
-            horizontal = { width = 0.99 }
+            horizontal = { width = 0.99 },
           },
         },
-      }
+      })
 
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
 
       vim.keymap.set("n", "<leader>fn", function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fn.stdpath("config")
-        }
+        require("telescope.builtin").find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
       end)
     end,
-  }
+  },
 }
